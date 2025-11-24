41d2914b660232e8e40a483ea31815bf0c761a15
gaunny <48590551+gaunny@users.noreply.github.com>
2025-11-24 11:18:44 +0800

    Update estimate.sh

--- estimate.sh
+++ estimate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# data_path should contain :dmri_data.nii.gz bvals.bvals bvecs.bvecs
+# data_path should contain :dmri_data.nii.gz bvals.bvals bvecs.bvecs wm_mask.nii.gz
+# Train the model
 python main.py \
-    --data_path /data1/your_subject/ \
+    --data_path /data/your_subject/ \
     --sh_degree 8 \
     --n_peaks 3 \
     --iteration 5 \
@@ -17,7 +18,7 @@ python main.py \
     --loss_intensity L2 \
     --intensity_weight 1 \
     --loss_oddsensitive L1 \
-    --oddsensitive_weight 1e-04 \
+    --oddsensitive_weight 5e-04 \
     --loss_smoothness cauchy \
     --smoothness_weight 1e-04 \
     --sigma_smoothness 1e-05\
@@ -29,4 +30,34 @@ python main.py \
     --graph_sampling healpix\
     --conv_name mixed \
     --pve_weight 1e-11
-    
+#Inference on subject. 
+python test_main.py \
+        --data_path /data/your_subject/ \
+        --n_peaks 3 \
+        --iteration 1 \
+        --batch_size 16 \
+        --lr 0.0017 \
+        --epoch 5 \
+        --filter_start 2 \
+        --sh_degree 8 \
+        --kernel_sizeSph 5 \
+        --kernel_sizeSpa 3 \
+        --depth 4 \
+        --n_side 16 \
+        --save_every 1 \
+        --loss_intensity L2 \
+        --intensity_weight 1 \
+        --loss_oddsensitive L1 \
+        --oddsensitive_weight 5e-04 \
+        --loss_smoothness cauchy \
+        --smoothness_weight 1e-04 \
+        --sigma_smoothness 1e-05\
+        --loss_non_negativity L2  \
+        --nn_fodf_weight 1 \
+        --wm \
+        --full_basis \
+        --patch_size 3 \
+        --graph_sampling healpix\
+        --conv_name mixed \
+        --pve_weight 1e-11 \
+        --load_state model_weights/hcp/model.pth
